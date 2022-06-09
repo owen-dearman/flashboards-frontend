9458d02d7e05b85bd29ca1d066c75f044e958c88
Owen Dearman <owendearman@gmail.com>
2022-06-09 09:12:12 +0100

    added prettier w/ scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <h1>Created By Owen Dearman</h1>
-  );
+  return <h1>Created By Owen Dearman</h1>;
 }
 
 export default App;
